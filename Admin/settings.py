59a31a4180553a7b2cd3705f6f6123539b5034b6
Shaxzod <shaxzod20000823@gmail.com>
2021-06-23 21:01:58 +0500

    project

--- Admin/settings.py
+++ Admin/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@2we!#ya**g24ci$i%v*@#m(e8e=t35-3&yr_!0g+i1kt@c@76
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
